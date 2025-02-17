5115e07fd47b522fb9e6a96071782d714c9e0f97
GenKrit <shashwatp.20@gmail.com>
2025-02-17 09:14:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import string
+import numpy as np
+import os
+from pickle import dump, load
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.applications.xception import Xception, preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical, get_file
+from keras.layers import add
+from keras.models import Model, load_model
+from keras.layers import Input,Dense
